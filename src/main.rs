30b9bbd614b95f8410c6f67ecb0941a760786a03
eske <eske.brun3@gmail.com>
2024-09-11 18:39:45 +0200

    basic read write

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 use crate::service::Service;
 use http_handler::HttpHandler;
@@ -14,7 +14,9 @@ mod service;
 
 #[tokio::main]
 async fn main() {
-    let state = Arc::new(Mutex::new(State {}));
+    let state = Arc::new(Mutex::new(State {
+        s: HashMap::new()
+    }));
     let mut handler = HttpHandler {
         inner: Router { state },
     };
@@ -27,8 +29,26 @@ async fn main() {
 }
 
 #[derive(Debug)]
-pub struct State {}
+pub struct State {
+    pub s: HashMap<String, String>
+}
 
 fn index() -> Box<dyn IntoResponse> {
     Box::new("hey from index")
+}
+
+fn read(state: Arc<Mutex<State>>, key: String) -> Box<dyn IntoResponse> {
+    if let Some(value) = state.lock().unwrap().s.get(&key) {
+        Box::new(value.to_string())
+    } else {
+        Box::new("key not found")
+    }
+}
+
+fn write(state: Arc<Mutex<State>>, key: String, value: String) -> Box<dyn IntoResponse> {
+    if let Some(old_value) = state.lock().unwrap().s.insert(key, value) {
+        Box::new(old_value)
+    } else {
+        Box::new(())
+    }
 }
--- src/router.rs
+++ src/router.rs
@@ -3,8 +3,9 @@ use std::sync::{Arc, Mutex};
 use crate::{
     index,
     into_response::IntoResponse,
+    read,
     service::{Fut, Service},
-    State,
+    write, State,
 };
 use tiny_http::Request;
 
@@ -17,16 +18,25 @@ impl Router {
     pub fn new() -> Self {
         todo!()
     }
-    fn route(&self, url: &str, method: &tiny_http::Method) -> Route {
+    fn route(&self, url: &str, method: &tiny_http::Method, body: String) -> Route {
         match (url, method) {
             ("/", tiny_http::Method::Get) => Route::GetIndex,
+            (url, tiny_http::Method::Get) if url.starts_with("/read/") => {
+                let key = url.strip_prefix("/read/").unwrap().to_string();
+                Route::GetRead { key }
+            }
+            (url, tiny_http::Method::Post) if url.starts_with("/write/") => {
+                let key = url.strip_prefix("/write/").unwrap().to_string();
+                Route::PostWrite { key, value: body }
+            }
             _ => Route::NotFound,
         }
     }
     async fn run_route(&self, route: Route) -> Result<Box<dyn IntoResponse>, ()> {
         match route {
             Route::GetIndex => Ok(index()),
-            Route::GetOther => todo!(),
+            Route::GetRead { key } => Ok(read(self.state.clone(), key)),
+            Route::PostWrite { key, value } => Ok(write(self.state.clone(), key, value)),
             Route::NotFound => todo!(),
         }
     }
@@ -39,9 +49,11 @@ impl Service<tiny_http::Request> for Router {
 
     type Future = Fut<Self::Response, Self::Error>;
 
-    fn call(&mut self, request: tiny_http::Request) -> Self::Future {
+    fn call(&mut self, mut request: tiny_http::Request) -> Self::Future {
+        let mut buf = String::new();
+        request.as_reader().read_to_string(&mut buf).unwrap();
         let this = self.clone();
-        let route = this.route(request.url(), request.method());
+        let route = this.route(request.url(), request.method(), buf);
         Box::pin(async move {
             let response = this.run_route(route).await.unwrap();
             Ok((request, response))
@@ -51,6 +63,7 @@ impl Service<tiny_http::Request> for Router {
 
 enum Route {
     GetIndex,
-    GetOther,
+    GetRead { key: String },
+    PostWrite { key: String, value: String },
     NotFound,
 }
