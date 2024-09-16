ce39951b64687c5595b34b3cca3d1120e0dcdd26
eske <eske.brun3@gmail.com>
2024-09-16 22:46:45 +0200

    insert billett route

--- src/bestilling/billett/mod.rs
+++ src/bestilling/billett/mod.rs
@@ -6,6 +6,8 @@ use poem_openapi::{
     ApiResponse, OpenApi,
 };
 
+use super::{bestilling_id, model::Bestilling};
+
 mod get_billett;
 mod lagre_billett;
 pub mod model;
@@ -15,6 +17,26 @@ mod query;
 enum IndexResponse {
     #[oai(status = 200)]
     Ok(PlainText<String>, #[oai(header = "Set-Cookie")] String),
+    #[oai(status = 500)]
+    Error,
+}
+
+#[derive(Debug, ApiResponse)]
+enum GetBillettResponse {
+    #[oai(status = 200)]
+    Ok(Json<model::Billett>),
+    #[oai(status = 404)]
+    NotFound,
+    #[oai(status = 500)]
+    InternalError,
+}
+
+#[derive(Debug, ApiResponse)]
+enum PostBilletResponse {
+    #[oai(status = 201)]
+    Ok,
+    #[oai(status = 500)]
+    InternalError,
 }
 
 pub struct BilletApi {
@@ -28,7 +50,12 @@ impl BilletApi {
         &self,
         #[oai(name = "bestilling_id")] bestilling_id: Cookie<Option<i32>>,
     ) -> IndexResponse {
-        let bestilling_id = bestilling_id.0.unwrap_or(1);
+        let Ok(bestilling_id) = BestillingsId::new(*bestilling_id)
+            .get_or_create(self.pool.clone())
+            .await
+        else {
+            return IndexResponse::Error;
+        };
         IndexResponse::Ok(
             PlainText("index billett".to_string()),
             format!("bestilling_id={bestilling_id}"),
@@ -45,17 +72,40 @@ impl BilletApi {
             Err(e) => {
                 dbg!(e);
                 GetBillettResponse::InternalError
-            },
+            }
+        }
+    }
+    #[oai(path = "/", method = "post")]
+    async fn post_billett(
+        &self,
+        #[oai(name = "bestillings_id")] bestilling_id: Cookie<Option<i32>>,
+    ) -> PostBilletResponse {
+        let Ok(bestilling_id) = BestillingsId::new(*bestilling_id)
+            .get_or_create(self.pool.clone())
+            .await
+        else {
+            return PostBilletResponse::InternalError;
+        };
+        match query::BillettQuery::new(self.pool.clone())
+            .insert_billet()
+            .await
+        {
+            Ok(_) => PostBilletResponse::Ok,
+            Err(_) => PostBilletResponse::InternalError,
         }
     }
 }
 
-#[derive(Debug, ApiResponse)]
-enum GetBillettResponse {
-    #[oai(status = 200)]
-    Ok(Json<model::Billett>),
-    #[oai(status = 404)]
-    NotFound,
-    #[oai(status = 500)]
-    InternalError,
+struct BestillingsId(Option<i32>);
+impl BestillingsId {
+    fn new(id: Option<i32>) -> Self {
+        Self(id)
+    }
+    async fn get_or_create(
+        &self,
+        _pool: Arc<sqlx::Pool<sqlx::Postgres>>,
+    ) -> Result<i32, sqlx::Error> {
+        dbg!("todo query db for id");
+        Ok(self.0.unwrap_or(1))
+    }
 }
--- src/bestilling/billett/query.rs
+++ src/bestilling/billett/query.rs
@@ -11,12 +11,17 @@ impl BillettQuery {
     }
 
     pub async fn get_billett_by_id(&self, id: i32) -> Result<Option<Billett>, sqlx::Error> {
-        dbg!("quering billett med id ", id);
-        dbg!(dbg!(
-        sqlx::query_as("select bestillings_id, fra_iata_code, til_iata_code, status::text, billett_type::text, timestamp::text from billett where bestillings_id = $1")
-            .bind(id)
-            .fetch_optional(&*self.pool)
-            .await
-    ))
+        sqlx::query_as(
+            "select bestillings_id, fra_iata_code, til_iata_code,
+                status::text, billett_type::text, timestamp::text
+                from billett
+                where bestillings_id = $1",
+        )
+        .bind(id)
+        .fetch_optional(&*self.pool)
+        .await
+    }
+    pub async fn insert_billet(&self) -> Result<(), sqlx::Error> {
+        todo!()
     }
 }
